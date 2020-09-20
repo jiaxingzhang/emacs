145dcb81f0ab738a0bb08909b8353f607de76f45
Jiaxing Zhang <jiaxingzhang@Jiaxings-MBP.fios-router.home>
2020-09-20 01:22:45 -0400

    ace jump

--- general.el
+++ general.el
@@ -282,3 +282,26 @@ searches all buffers."
 (global-set-key (kbd "C-c f A") 'origami-open-all-nodes)
 (global-set-key (kbd "C-c f a") 'origami-close-all-nodes)
 
+
+;;
+;; ace jump mode major function
+;; 
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+;; you can select the key you prefer to
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+;; 
+;; enable a more powerful jump back function from ace jump mode
+;;
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
