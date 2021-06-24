f122af6ff69ddc9c27fdf03156cb182167cd8945
Jiaxing Zhang <jiaxingzhang@gmail.com>
2021-06-24 15:17:32 -0400

    enable helm tab in terminal

--- general.el
+++ general.el
@@ -149,7 +149,7 @@ try-expand-whole-kill))
 (require 'helm-config)
 (require 'helm-ag)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
-;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 ;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 ;; (helm-mode 0)
 ;; (global-set-key (kbd "C-c x") 'helm-mode)
